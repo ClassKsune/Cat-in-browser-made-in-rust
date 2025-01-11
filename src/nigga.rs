586d76156eb79cc2bc31c5e0adec0fd0a55d7a45
Krystof Novotny <felixmorningstar@proton.me>
2025-01-11 20:09:39 +0100

    Delete src/u

--- src/nigga.rs
+++
@@ -1,9 +0,0 @@
-use std::io;
-
-pub fn nigga() -> io::Result<()> {
-    let mut buffer = String::new();
-    let stdin = io::stdin(); // We get `Stdin` here
-    stdin.read_line(&mut buffer)?;
-    let output = buffer.trim();
-    Ok(())
-}
